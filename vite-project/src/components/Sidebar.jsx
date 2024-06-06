9d5cd81dcffa5ad32d40d071feda000c9a5ef4a9
Aqdas Zishan <zishanaqdas@gmail.com>
2024-06-06 14:03:23 +0530

    f

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -3,13 +3,13 @@ import './Sidebar.css';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <h1>E-Learning</h1>
-      <nav>
-        <a href="#">Dashboard</a>
-        <a href="#">Browse</a>
-      </nav>
-    </div>
+      <div className="sidebar">
+        <h1>E-Learning</h1>
+        <nav>
+          <a href="#">Dashboard</a>
+          <a href="#">Browse</a>
+        </nav>
+      </div>
   );
 }
 
