2f9d9352bdfd103ab66e91de9e124500aac856dc
MohitSaraswat17 <145154965+MohitSaraswat17@users.noreply.github.com>
2024-06-06 13:47:18 +0530

    Commit

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -6,9 +6,10 @@ import { sendToken } from '../utils/jwtToken.js';
 export const register = catchAsyncErrors(async (req, res, next) => {
     const { name, email, phone, password, role } = req.body;
     
-    // if(!name||!email||!phone||!password||!role){
-    //     return next(new ErrorHandler("All Fields are Required!",400));
-    // }
+    if(!name||!email||!phone||!password||!role){
+        return next(new ErrorHandler("All Fields are Required!",400));
+    }
+    
 
     let user = await User.findOne({ email });
 
